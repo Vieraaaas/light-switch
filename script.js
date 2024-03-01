216ef2fb032242c5d2395cc4aabf3b966a5c8950
Claas Paaschburg <paaschburgc@gmail.com>
2024-03-01 13:01:22 +0100

    feat: change title on click

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
+const title = document.querySelector("title");
 
 function pressButton() {
   button.classList.toggle("btn-off");
   body.classList.toggle("content-dark");
+  if (title.innerText === "Good Morning") {
+    title.innerText = "Good Night";
+  } else {
+    title.innerText = "Good Morning";
+  }
 }
 
 button.addEventListener("click", pressButton);
